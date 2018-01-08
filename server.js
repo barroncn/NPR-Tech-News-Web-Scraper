9960e47df762e80219dd10472bf61b30e2db66ba
Caitlin Barron <barroncn@gmail.com>
2018-01-08 00:25:51 +0000

    port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var exphbs = require("express-handlebars");
 // Require all models
 var db = require("./models");
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
